3ca6f89b5e96beef7233567cfc76458e60e1c333
DieroKreator <qtechwired@gmail.com>
2025-10-15 16:10:26 -0300

    add register test with fake data

---
+++ playwright/e2e/auth/register.spec.js
@@ -0,0 +1,32 @@
+// @ts-check
+import { test, expect } from '@playwright/test'
+import { faker } from '@faker-js/faker'
+
+test.describe('POST /auth/resgister', () => {
+  test('deve cadastrar um novo usuário', async ({ request }) => {
+
+    const firstName = faker.person.firstName()
+    const lastName = faker.person.lastName()
+
+    const user = {
+      // name: firstName + ' ' + lastName,
+      name: `${firstName} ${lastName}`, // interpolation
+      email: faker.internet.email({ firstName, lastName }).toLowerCase(),
+      password: "123456"
+    }
+
+    const response = await request.post('http://localhost:3333/api/auth/register', {
+      data: user
+    })
+
+    expect(response.status()).toBe(201)
+
+    const responseBody = await response.json()
+    // expect(responseBody.message).toBe('Usuário cadastrado com sucesso!')
+    expect(responseBody).toHaveProperty('message', 'Usuário cadastrado com sucesso!')
+    expect(responseBody.user).toHaveProperty('id')
+    expect(responseBody.user).toHaveProperty('name', user.name)
+    expect(responseBody.user).toHaveProperty('email', user.email)
+    expect(responseBody.user).not.toHaveProperty('password')
+  })
+})
