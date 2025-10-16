57c9dbeb2dd9d126c4573a789c11689edb715946
DieroKreator <qtechwired@gmail.com>
2025-10-16 16:23:35 -0300

    increase test coverage for links and clean code

--- playwright/e2e/links/post.spec.js
+++ playwright/e2e/links/post.spec.js
@@ -2,22 +2,28 @@ import { test, expect } from '@playwright/test'
 import { authService } from '../../support/services/auth'
 import { linkService } from '../../support/services/links'
 import { getUserWithLink } from '../../support/factories/user'
+import { title } from 'process'
 
 test.describe('POST /api/links', () => {
 
-    test('deve encurtar um novo link', async ({ request }) => {
+    const user = getUserWithLink()
 
-        const auth = authService(request)
-        const link = linkService(request)
+    let auth
+    let link
+    let token
 
-        const user = getUserWithLink()
+    test.beforeEach(async ({ request }) => {
+        auth = authService(request)
+        link = linkService(request)
 
         await auth.createUser(user)
-        const token = await auth.getToken(user)
+        token = await auth.getToken(user)
+    })
+
+    test('deve encurtar um novo link', async ({ request }) => {
         const response = await link.createLink(user.link, token)
 
         expect(response.status()).toBe(201)
-
         const { data, message } = await response.json()
 
         expect(data).toHaveProperty('id')
@@ -27,4 +33,33 @@ test.describe('POST /api/links', () => {
         expect(message).toBe('Link criado com sucesso')
     })
 
+    test('não deve encurtar quando a url original não é informada', async () => {
+        const response = await link.createLink({ ...user.link, original_url: '' }, token)
+
+        expect(response.status()).toBe(400)
+        const { message } = await response.json()
+
+        expect(message).toBe('O campo \'OriginalURL\' é obrigatório')
+        console.log(message)
+    })
+
+    test('não deve encurtar quando o titulo não é informado', async () => {
+        const response = await link.createLink({ ...user.link, title: '' }, token)
+
+        expect(response.status()).toBe(400)
+        const { message } = await response.json()
+
+        expect(message).toBe('O campo \'Title\' é obrigatório')
+        console.log(message)
+    })
+
+    test('não deve encurtar quando a url original é invalida', async () => {
+        const response = await link.createLink({ ...user.link, original_url: 'teste@teste.com.br' }, token)
+
+        expect(response.status()).toBe(400)
+        const { message } = await response.json()
+
+        expect(message).toBe('O campo \'OriginalURL\' deve ser uma URL válida')
+        console.log(message)
+    })
 })
