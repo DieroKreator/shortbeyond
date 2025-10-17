663e1eb345ba6f18224a9d2a64ffa892edf86507
DieroKreator <qtechwired@gmail.com>
2025-10-17 15:11:22 -0300

    Add validations on links/get test

--- playwright/e2e/links/get.spec.js
+++ playwright/e2e/links/get.spec.js
@@ -1,14 +1,33 @@
-import {test, expect} from '../../support/fixtures'
+import { test, expect } from '../../support/fixtures'
 
-import {getUserWithLinks} from '../../support/factories/user'
+import { getUserWithLinks } from '../../support/factories/user'
 
-test('dev retornar uma lista de links pre-encurtados', async ({auth, links}) => {
+test('dev retornar uma lista de links pre-encurtados', async ({ auth, links }) => {
     const user = getUserWithLinks()
 
     await auth.createUser(user)
     const token = await auth.getToken(user)
 
     for (const link of user.links) {
-        await links.createLink(link,token)
+        await links.createLink(link, token)
+    }
+
+    const response = await links.getLinks(token)
+
+    expect(response.status()).toBe(200)
+
+    const body = await response.json()
+
+    expect(body.message).toBe('Links Encurtados')
+    expect(body.count).toBe(user.links.length)
+    expect(Array.isArray(body.data)).toBeTruthy()
+
+    for (const [index, link] of body.data.entries()) {
+        expect(link).toHaveProperty('id')
+        expect(link).toHaveProperty('original_url', user.links[index].original_url)
+        expect(link).toHaveProperty('short_code')
+        expect(link).toHaveProperty('title', user.links[index].title)
+
+        expect(link.short_code).toMatch(/^[a-zA-Z0-9]{5}$/)
     }
 })
